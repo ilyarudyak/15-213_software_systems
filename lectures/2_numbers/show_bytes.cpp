48bc50bdc2cb4d481ed461f424e56ae4a9148602
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-08-26 22:19:41 +0300

    misc changes

--- lectures/2_numbers/show_bytes.cpp
+++ lectures/2_numbers/show_bytes.cpp
@@ -101,7 +101,7 @@ void show_twocomp() {
 }
 
 int main(int argc, char *argv[]) {
-    int val = 12345;
+    int val = 0x12345678;
 
     if (argc > 1) {
         if (argc > 1) {
@@ -110,18 +110,19 @@ int main(int argc, char *argv[]) {
         printf("calling test_show_bytes\n");
         test_show_bytes(val);
     } else {
-        printf("calling show_twocomp\n");
-        show_twocomp();
-        printf("Calling simple_show_a\n");
-        simple_show_a();
-        printf("Calling simple_show_b\n");
-        simple_show_b();
-        printf("Calling float_eg\n");
-        float_eg();
-        printf("Calling string_ueg\n");
-        string_ueg();
-        printf("Calling string_leg\n");
-        string_leg();
+        show_bytes((byte_pointer) &val, sizeof(int));
+//        printf("calling show_twocomp\n");
+//        show_twocomp();
+//        printf("Calling simple_show_a\n");
+//        simple_show_a();
+//        printf("Calling simple_show_b\n");
+//        simple_show_b();
+//        printf("Calling float_eg\n");
+//        float_eg();
+//        printf("Calling string_ueg\n");
+//        string_ueg();
+//        printf("Calling string_leg\n");
+//        string_leg();
     }
     return 0;
 }
